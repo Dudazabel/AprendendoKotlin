80332ec95b205b23ce5940e0e759bd5f1e05cbc1
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-02-11 20:09:40 -0300

    MZ feat: implementa desafios propostos

--- app/src/main/java/com/example/helloword/MainActivity.kt
+++ app/src/main/java/com/example/helloword/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,22 +15,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.VerticalAlignmentLine
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.helloword.ui.theme.HelloWordTheme
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,9 +51,18 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .size(320.dp),
                         shape = RoundedCornerShape(16.dp),
+                        shadowElevation = 10.dp,
                     ){
                         Column(modifier = Modifier.padding(24.dp),
                             horizontalAlignment = Alignment.CenterHorizontally) {
+
+                            val image = painterResource(R.drawable.avatar)
+                            Image(
+                                painter = image,
+                                contentDescription = null
+                            )
+                            Spacer(modifier = Modifier.padding(18.dp))
+
                             Text(
                                 text = "Guerreiro Kotlin",
                                 color = Color.Black,
@@ -81,7 +90,6 @@ class MainActivity : ComponentActivity() {
                                     color = Color.Red,
                                     fontSize = 18.sp,
                                     fontWeight = FontWeight.Bold,
-
                                     )
                             }
                             Spacer(modifier = Modifier.padding(top = 24.dp))
@@ -90,6 +98,17 @@ class MainActivity : ComponentActivity() {
                                 fontSize = 14.sp,
                                 textAlign = TextAlign.Center,
                             )
+                            Button(
+                                onClick = { /*TODO*/ },
+                                modifier = Modifier
+                                    .padding(top = 18.dp, bottom = 18.dp),
+                                    colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF2B2D42),
+                                    )
+                                ) { Text(
+                                    text = "JOGAR",
+                                )
+                            }
                         }
                     }
 
@@ -98,6 +117,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
 
 @Composable
